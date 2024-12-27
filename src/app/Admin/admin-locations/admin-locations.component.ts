264174d7519fa6875adf821e3505d0451f2ba006
niranjanreddy2003 <venkatniranjan.t@gmail.com>
2024-12-27 15:21:15 +0530

    sixth

--- src/app/Admin/admin-locations/admin-locations.component.ts
+++ src/app/Admin/admin-locations/admin-locations.component.ts
@@ -76,14 +76,14 @@ export class AdminLocationsComponent implements OnInit, AfterViewInit {
 
   ngOnInit(): void {
     // Use default marker icon
-    L.Marker.prototype.options.icon = L.icon({
-      iconUrl: 'assets/leaflet-images/marker-icon.png',
-      shadowUrl: 'assets/leaflet-images/marker-shadow.png',
-      iconSize: [25, 41],
-      iconAnchor: [12, 41],
-      popupAnchor: [1, -34],
-      shadowSize: [41, 41]
-    });
+    // L.Marker.prototype.options.icon = L.icon({
+    //   // iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png',
+    //   // shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png',
+    //   iconSize: [25, 41],
+    //   iconAnchor: [12, 41],
+    //   popupAnchor: [1, -34],
+    //   shadowSize: [41, 41]
+    // });
 
     // Fetch existing routes
     this.fetchRoutes();
