1c9abe19d131b971916c496d54492b5fab095720
niranjanreddy2003 <venkatniranjan.t@gmail.com>
2024-12-27 14:50:17 +0530

    ffith

--- src/app/Admin/admin-locations/admin-locations.component.ts
+++ src/app/Admin/admin-locations/admin-locations.component.ts
@@ -77,8 +77,8 @@ export class AdminLocationsComponent implements OnInit, AfterViewInit {
   ngOnInit(): void {
     // Use default marker icon
     L.Marker.prototype.options.icon = L.icon({
-      iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png',
-      shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png',
+      iconUrl: 'assets/leaflet-images/marker-icon.png',
+      shadowUrl: 'assets/leaflet-images/marker-shadow.png',
       iconSize: [25, 41],
       iconAnchor: [12, 41],
       popupAnchor: [1, -34],
